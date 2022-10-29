8a969664dfffaac9c17421e45327bdf75d6faac9
Piotr Paczuski <piotr-paczuski@o2.pl>
2022-10-29 04:59:00 +0200

    done task button added

--- JS/script.js
+++ JS/script.js
@@ -8,7 +8,7 @@
             name: "Zrobić pracę domową!",
             done: true,
         },
-    ]
+    ];
 
     const addNewTask = () => {
         const newTask = document.querySelector(".js-newTask").value.trim();
@@ -21,25 +21,44 @@
         })
 
         render();
-    }
+    };
 
-    const removeTask = (task) => {
-        tasks.pop({
-            name: task,
-        })
+    const removeTask = (taskIndex) => {
+        tasks.splice(taskIndex, 1);
+        render();
+    };
 
+    const toggleDoneTask = (taskIndex) => {
+        tasks[taskIndex].done = !tasks[taskIndex].done;
         render();
     }
 
     const onFormButton = () => {
-        const button = document.querySelector(".js-button");
 
+        const button = document.querySelector(".js-button");
         button.addEventListener("click", (event) => {
             event.preventDefault();
             addNewTask();
+        });
+    }
+
+    const eventButtons = () => {
+        const doneButton = document.querySelectorAll(".js-done");
+        doneButton.forEach((doneButton, taskIndex) => {
+            doneButton.addEventListener("click", (event) => {
+                event.preventDefault();
+                toggleDoneTask(taskIndex);
+            })
         })
 
-        
+        const removeButton = document.querySelectorAll(".js-remove");
+        removeButton.forEach((removeButton, taskIndex) => {
+            removeButton.addEventListener("click", (event) => {
+                event.preventDefault();
+                removeTask(taskIndex);
+            });
+        });
+
     }
 
     const render = () => {
@@ -48,18 +67,21 @@
         for (const task of tasks){
             htmlString += `
             <li${task.done ? " class=\"content__done\"" : " class=\"\"" }>
+            <button class="js-done">Zrobione</button>
                 ${task.name}
+            <button class="js-remove">Usuń zadanie</button>
             </li>
-            <button>Usuń zadanie</button>
             `;
-        }
+        };
 
         document.querySelector(".js-tasks").innerHTML = htmlString;
-    }
+
+        eventButtons();
+    };
 
     const init = () => {
         onFormButton();
-    }
+    };
 
     init();
 }
